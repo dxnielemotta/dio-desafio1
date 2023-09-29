45a07927f83fe4fdafbbdc112d11655d5ec35252
dxnielemotta <danielemottarocha@gmail.com>
2023-09-28 23:16:02 -0300

    estabelecendo condições

--- index.js
+++ index.js
@@ -12,5 +12,31 @@ let quantidadeXP
 let nivelHeroi 
 const menssagem = "O Herói de nome" + nomeHeroi + "está no nível " + nivelHeroi
 
+if (quantidadeXP <1.000) {
+    nivelHeroi= "Bronze"
+    console.log (menssagem)
+}  else if ((quantidadeXP >=1.001) && (quantidadeXP <=2.000)) {
+    nivelHeroi = "Ferro"
+    console.log (menssagem)
+} else if ((quantidadeXP >=2.001) && (quantidadeXP <=5.000)) {
+    nivelHeroi = "Prata"
+    console.log (menssagem)
+} else if ((quantidadeXP >=6.001) && (quantidadeXP <=7.000)) {
+    nivelHeroi = "Ouro"
+    console.log (menssagem)
+} else if ((quantidadeXP >=7.001) && (quantidadeXP <=8.000)) {
+    nivelHeroi = "Platina"
+    console.log (menssagem)
+} else if ((quantidadeXP >=8.001) && (quantidadeXP <=9.000)) {
+    nivelHeroi = "Ascendente"
+    console.log (menssagem)
+}  else if ((quantidadeXP >=9.001) && (quantidadeXP <=10.000)) {
+    nivelHeroi = "Imortal"
+    console.log (menssagem)
+} else {
+    nivelHeroi = "Radiante"
+    console.log (menssagem)
+}
+
 
 
